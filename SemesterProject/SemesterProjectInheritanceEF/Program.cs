95b048873f956bd487560981b4114fa8a740b10c
cph57689 <pernillejaco@gmail.com>
2016-04-27 13:58:12 +0200

    First commit Inheritance EF virker

---
+++ SemesterProject/SemesterProjectInheritanceEF/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProjectInheritanceEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var student = new Student
+                {
+                    FirstMidName = "Jeanette",
+                    LastName = "Borring-Møller",
+                    EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                };
+
+                context.People.Add(student);
+
+                var student1 = new Student
+                {
+                    FirstMidName = "Pernille",
+                    LastName = "Jacobsen",
+                    EnrollmentDate = DateTime.Parse(DateTime.Today.ToString())
+                };
+                
+                context.People.Add(student1);
+
+                var techaer = new Teacher
+                {
+                    FirstMidName = "Anders",
+                    LastName = "Kalhauge",
+                    HireDate = DateTime.Parse(DateTime.Today.ToString())
+                };
+
+                context.People.Add(techaer);
+
+                var techaer1 = new Teacher
+                {
+                    FirstMidName = "Anden",
+                    LastName = "Lærer",
+                    HireDate = DateTime.Parse(DateTime.Today.ToString())
+                };
+
+                context.People.Add(techaer1);
+                context.SaveChanges();
+            }
+        }
+    }
+    public partial class Person
+    {
+        public int ID { get; set; }
+        public string FirstMidName { get; set; }
+        public string LastName { get; set; }
+    }
+    public partial class Student : Person
+    {
+        public System.DateTime EnrollmentDate { get; set; }
+    }
+    public partial class Teacher : Person
+    {
+        public System.DateTime HireDate { get; set; }
+    }
+    public partial class SchoolContext : DbContext
+    {
+        public static string CONN =
+    @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SCHOOLDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //@"Data Source=(localdb)\mssqllocaldb;Integrated Security=True";  
+
+        public SchoolContext() : base(CONN)
+        {
+            Database.SetInitializer<SchoolContext>(
+          //new DropCreateDatabaseAlways<PetClubContext>()
+          new DropCreateDatabaseAlways<SchoolContext>()
+          );
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+    }
+}
---
+++ SemesterProject/SemesterProjectInheritanceFluentAPI/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterProjectInheritanceFluentAPI
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().Property(s ⇒ s.FirstMidName)
+            .HasColumnName("FirstName");
+        }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Enrollment> Enrollments { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+    }
+    /*
+    public class MyContext : DbContext
+    {
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().Property(s ⇒ s.FirstMidName)
+            .HasColumnName("FirstName");
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Enrollment> Enrollments { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+    }
+    */
+
+}
+
